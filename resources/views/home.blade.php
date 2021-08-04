0ada901c163992b5fd75da287212719649308aba
abelinaaulia <abelinaaulia@gmail.com>
2021-08-04 13:21:38 +0700

    membuat halaman home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width= , initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>HOME</title>
+</head>
+<body>
+    <h1>Halaman Home</h1>
+    <h3>Risma Septiani Fadila</h3>
+    <p>Rismlaf651@gmail.com</p>
+    <img src="risma.jpg" alt="risma" width="200">
+</body>
+</html>
