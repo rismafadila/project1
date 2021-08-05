16a5b838576b969ebb64d51343cacdb535647828
rismafadila <rismaf651@gmail.com>
2021-08-05 08:33:37 +0700

    halaman about

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width= , initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>about</title>
+</head>
+<body>
+    <h1>Halaman about</h1>
+    <h3>Risma Septiani Fadila</h3>
+    <p>Rismlaf651@gmail.com</p>
+    <img src="risma.jpg" alt="risma" width="200">
+</body>
+</html>
